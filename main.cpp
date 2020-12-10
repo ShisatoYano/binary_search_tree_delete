82784ae3bdd12ef188fb7007b47722960dff7257
Shisato <shisatoyano@gmail.com>
2020-12-10 23:13:38 +0900

    implemented node structure

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+    int key;
+    struct node *left, *right;
+};
+
+// create new function
+struct node* new_node(int key) {
+    struct node* tmp = (struct node*)malloc(sizeof(struct node));
+    tmp->key = key;
+    tmp->left = tmp->right = NULL;
+    return tmp;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
